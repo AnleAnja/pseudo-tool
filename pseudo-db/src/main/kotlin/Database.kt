e5d14a5747f44829bd0a47f8178501e18acbdccc
AnleAnja <anja_katharina.bertels@smail.th-koeln.de>
2023-05-19 00:17:53 +0200

    error handling

--- pseudo-db/src/main/kotlin/Database.kt
+++ pseudo-db/src/main/kotlin/Database.kt
@@ -1,9 +1,11 @@
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 
-class DatabaseConnection {
-    fun init(environment: Environment) {
+object DatabaseConnection {
+    fun init() {
+        val environment = Environment
         val database = Database.connect(
             "jdbc:postgresql://${environment.serverName()}:5432/${environment.dbName()}",
             driver = "org.postgresql.Driver",
@@ -11,19 +13,24 @@ class DatabaseConnection {
             password = environment.password()
         )
         if (environment.createDb()) {
-            transaction(database) {
-                val name1 = randomString()
-                val name2 = randomString()
-                val id = randomId()
-                Users.update {
-                    it[system_id] = id
-                    it[lastname] = randomName(name1)
-                    it[firstname] = randomName(name2)
-                    it[email] = randomEmail(name2, name1)
-                    it[registration] = randomRegistration()
-                    it[campus_id] = id
+            try {
+                transaction(database) {
+                    val name1 = randomString()
+                    val name2 = randomString()
+                    val id = randomId()
+                    Users.update {
+                        it[system_id] = id
+                        it[lastname] = randomName(name1)
+                        it[firstname] = randomName(name2)
+                        it[email] = randomEmail(name2, name1)
+                        it[registration] = randomRegistration()
+                        it[campus_id] = id
+                    }
                 }
-            }
+            } catch (e: ExposedSQLException) {
+            println(e.message)
+            e.printStackTrace()
+        }
         }
     }
 }
